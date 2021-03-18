d18dc8472a404079ee0daa1def5145e66417b34c
bernisnukic <44529204+bernisnukic@users.noreply.github.com>
2021-03-18 16:48:12 +0100

    add menu to projects page

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -1,19 +1,45 @@
-import React from "react";
+import React, {useState} from "react";
 
 import Header from '../../components/header'
 
 
 export default function Home() {
+
+    const [menuOpen, setMenuOpen] = useState(false);
+
+
     return (
         <div className="min-h-screen font-inter flex justify-center bg-white antialiased font-sans">
             <div className="flex w-full flex-col max-w-5xl mx-auto px-6 md:px-8 mt-10">
                 <div className="flex flex-col items-start">
-                    <div className="text-lg sm:text-xl md:text-2xl lg:text-3xl text-gray-900 font-medium md:font-normal leading-none">Bernis Nukic</div>
-                    <div className="flex w-full">
+
+                    <div className="flex w-full items-center justify-between">
+                        <div className="text-lg sm:text-xl md:text-2xl lg:text-3xl text-gray-900 font-medium md:font-normal leading-none">Bernis Nukic</div>
+
+                        {!menuOpen ? (
+                            <svg onClick={() => setMenuOpen(true)} className="sm:hidden h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16"/>
+                            </svg>
+                        ) : (
+                            <svg onClick={() => setMenuOpen(false)} className="sm:hidden h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12"/>
+                            </svg>
+                        )}
+
+                    </div>
+
+                    <div className="hidden sm:block flex w-full">
 
                         <Header/>
 
                     </div>
+
+                    <div className="sm:hidden flex w-full">
+
+                        {menuOpen && <Header/>}
+
+                    </div>
+
                     <div className="mt-10 w-full">
                         <div className="bg-white">
                             <div className="flex flex-col sm:flex-row justify-between">
